d3fa7c5762a218d5325fc739924a53e1c1f1b502
RoundStarling20 <88243850+RoundStarling20@users.noreply.github.com>
2021-08-03 22:48:38 -0400

    Ban, unban fixes
    Made ban and unban work on users not present in server

--- cogs/mod.py
+++ cogs/mod.py
@@ -16,26 +16,15 @@ class TestCog(commands.Cog):
 
     @commands.command()
     @commands.check(custom.isItme)
-    async def ban(self, ctx, member: discord.Member = None, *, reason = None):
-        if member != None:
-            await member.ban(reason = reason)
-            await ctx.send(f'Banned {member.name}#{member.discriminator}')
+    async def ban(self, ctx, user: discord.abc.User, *, reason = None):
+            await ctx.guild.ban(user)
+            await ctx.send(f'Banned {user.name}#{user.discriminator} ')
 
-    #@commands.command()
-    #@commands.check(custom.isItme)
-    #async def unban(self, ctx, *, member): 
-    #    bannedUsers = await ctx.guild.bans()
-    #    member_name, member_discriminator = member.split('#')
-    #    for ban_entry in bannedUsers:
-    #        user = ban_entry.user
-    #        if (user.name, user.discriminator) == (member_name, member_discriminator):
-    #            await ctx.guild.unban(user)
-    #            await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
-    #            return
-    #        elif str(user.id) == member:
-    #            await ctx.guild.unban(user)
-    #            await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
-    #            return
+    @commands.command()
+    @commands.check(custom.isItme)
+    async def unban(self, ctx, user: discord.abc.User):
+        await ctx.guild.unban(user)
+        await ctx.send(f'Unbanned {user.name}#{user.discriminator} ')
                 
     @commands.command()
     @commands.check(custom.isItme)
@@ -45,11 +34,11 @@ class TestCog(commands.Cog):
             if message not in db["bannedEmojis"]:
                 db["bannedEmojis"].append(message)
                 custom.save_db(db)
-                await ctx.send(f'{[message]} has been forbidden')
+                await ctx.message.add_reaction('<a:yes:820523959878418452>')
         elif message not in db["bannedWords"]:
             db["bannedWords"].append(message)
             custom.save_db(db)
-            await ctx.send(f'{[message]} has been forbidden')
+            await ctx.message.add_reaction('<a:yes:820523959878418452>')
 
     @commands.command()
     @commands.check(custom.isItme)
@@ -59,20 +48,15 @@ class TestCog(commands.Cog):
             if message in db["bannedEmojis"]:
                 db["bannedEmojis"].remove(message)
                 custom.save_db(db)
-                await ctx.send(f'{[message]} has been unforbidden')
+                await ctx.message.add_reaction('<a:yes:820523959878418452>')
         elif message in db["bannedWords"]:
             db["bannedWords"].remove(message)
             custom.save_db(db)
-            await ctx.send(f'{[message]} has been unforbidden')
+            await ctx.message.add_reaction('<a:yes:820523959878418452>')
 
     @commands.Cog.listener()
     async def on_message(self, message):
         db = custom.get_db()
-    #    phraseSaid = message.content.lower().split()
-    #    for x in range(0, len(phraseSaid)):
-    #        if phraseSaid[x] in db["bannedEmojis"] + db["bannedWords"]:
-    #            await message.add_reaction('<a:no:820524004594024459>')
-    #            break
         for x in range(0, len(db["bannedWords"])):
             if db["bannedWords"][x] in message.content.lower():
                 await message.add_reaction('<a:no:820524004594024459>')
@@ -82,7 +66,6 @@ class TestCog(commands.Cog):
     async def clear(self, ctx, amount = 5):
         await ctx.channel.purge(limit = amount + 1)
         
-#ban and kick event
 
 def setup(client):
     client.add_cog(TestCog(client))
--- custom.py
+++ custom.py
@@ -6,6 +6,9 @@ import json
 def isItme(ctx):
     return ctx.message.author.id == 220327217312432129
 
+def isItyo(ctx):
+    return ctx.message.author.id == 870467770846945290
+
 def get_db():
     with open("cogs\database.json", "r") as file:
         return json.load(file)
